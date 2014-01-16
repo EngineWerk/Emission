b76c819e638f1c086cebfa8edd035d20d2a5122d
Paweł Czyżewski <pawel.czyzewski@enginewerk.com>
2014-01-16 22:22:45 +0100

    Added AppResponse class for Json responses

---
+++ src/Enginewerk/EmissionBundle/Response/AppResponse.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Enginewerk\EmissionBundle\Response;
+
+/**
+ * Description of AppResponse
+ *
+ * @author Paweł Czyżewski <pawel.czyzewski@enginewerk.com>
+ */
+class AppResponse
+{
+    private $message = null;
+    private $status = null;
+    private $data = null;
+    
+    public $response = array();
+    
+    /**
+     * Sets status on Error and response message
+     * 
+     * @param type $message
+     */
+    public function error($message = null)
+    {
+        $this->message = $message;
+        $this->status = 'Error';
+    }
+    
+    /**
+     * Sets status on Success and response message
+     * 
+     * @param type $message
+     */
+    public function success($message = null)
+    {
+        $this->message = $message;
+        $this->status = 'Success';
+    }
+    
+    /**
+     * Sets status on Info and response message
+     * 
+     * @param type $message
+     */
+    public function info($message = null)
+    {
+        $this->message = $message;
+        $this->status = 'Info';
+    }
+    
+    /**
+     * Sets response data
+     * 
+     * @param type $data
+     */
+    public function data($data)
+    {
+        $this->data = $data;
+    }
+    
+    private function prepareResponseStatus()
+    {
+        $this->response['status'] = $this->status;
+    }
+    
+    private function prepareResponseMessage()
+    {
+        $this->response['message'] = $this->message;
+    }
+    
+    private function prepareResponseData()
+    {
+        $this->response['data'] = $this->data;
+    }
+    
+    /**
+     * Sets response data and returns $this
+     * 
+     * @return \Enginewerk\EmissionBundle\Response\AppResponse
+     */
+    public function response()
+    {
+        ($this->message) ? $this->prepareResponseMessage() : '';
+        ($this->data) ? $this->prepareResponseData() : '';
+        ($this->status) ? $this->prepareResponseStatus() : '';
+        
+        return $this;
+    }
+}
