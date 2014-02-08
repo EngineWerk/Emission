03b6e90c9011ca14ce0002e25f90e213c491788a
Paweł Czyżewski <pawel.czyzewski@enginewerk.com>
2014-02-09 00:49:13 +0100

    Added Simple search for files

---
+++ src/Enginewerk/EmissionBundle/Resources/public/js/simple-search.js
@@ -0,0 +1,82 @@
+$(function(){
+    
+    var listFiltered = false;
+    var fileList = $('#filesTable tr');
+    
+    $(document).keyup(function(event) {
+        
+        if (event.keyCode === 27) { 
+            $('#simpleSearchInput').blur();
+            
+            if (listFiltered) {
+                resetFilter();
+            }
+        }
+    });
+    
+    $('#simpleSearchInput').change(function(){
+        info('Simple search', 'onChange');
+        filter();
+    });
+    $('#simpleSearchInput').keyup(function(){
+        info('Simple search', 'KeyUp');
+        filter();
+    });
+    
+    filter = function(){
+        var query = $('#simpleSearchInput').val();
+        info('Simple search', query);
+        
+        fileList.each(function() {
+            
+            found = $(this).attr('data-search').toLowerCase().search(query);
+
+            if (found >= 0) {
+                info('found', $(this).attr('data-search'));
+                info('query', query);
+                $(this).css('display', 'table-row');
+            } else {
+                $(this).css('display', 'none');
+            }
+        });
+        
+        listFiltered = true;
+    };
+    
+    resetFilter = function(){
+        
+        $('#simpleSearchInput').val('');
+        
+        fileList.each(function(){
+            $(this).css('display', 'table-row');
+        });
+        
+        listFiltered = false;
+    };
+    
+    decorate = function(row, decorate) {
+        log(decorate);
+        if (decorate && row) {
+            var text = row.children('td:first').html();
+            info('text', text);
+            row.children('td:first').html(text.replace(decorate, '<b>' + decorate + '</b>'));
+        }
+    };
+});
+
+var simpleSearchKeyDownControll = function(e) {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) { 
+        $('#emissionSimpleSearchContainer').toggle();
+        e.preventDefault();
+    }
+};
+
+function simpleSearchEnable() {
+    window.addEventListener("keydown", simpleSearchKeyDownControll);
+}
+
+function simpleSearchDisable() {
+    window.removeEventListener("keydown", simpleSearchKeyDownControll);
+}
+
+
