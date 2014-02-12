de0e3984bc85175e68be9206f6541e351045b62f
Paweł Czyżewski <pawel.czyzewski@enginewerk.com>
2014-02-12 21:26:21 +0100

    cs-fix

--- src/Enginewerk/EmissionBundle/Command/RemoveExpiredFilesCommand.php
+++ src/Enginewerk/EmissionBundle/Command/RemoveExpiredFilesCommand.php
@@ -3,10 +3,8 @@
 namespace Enginewerk\EmissionBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
 
 /**
  * Description of RemoveExpiredFilesCommand
@@ -22,19 +20,19 @@ class RemoveExpiredFilesCommand extends ContainerAwareCommand
             ->setDescription('Removes expired files')
         ;
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $date = new \DateTime('now');
         $output->writeln($date->format('Y-m-d H:i:s'));
-        
+
         $repository = $this->getContainer()->get('doctrine')->getRepository('EnginewerkEmissionBundle:File');
-        $em = $this->getContainer()->get('doctrine')->getManager();        
+        $em = $this->getContainer()->get('doctrine')->getManager();
         $files = $repository->getExpiredFiles();
-        
-        foreach($files as $file) {
+
+        foreach ($files as $file) {
             $output->writeln($file->getName());
-            try{
+            try {
                 // Nie działa z usługą BBS
                 $em->remove($file);
                 $em->flush();
