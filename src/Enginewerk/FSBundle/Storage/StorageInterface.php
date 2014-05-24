8e0b421a844b1ee7c1c1449f4ab54281820fce26
pawski <pawel.czyzewski@enginewerk.com>
2014-05-24 13:32:29 +0200

    php-cs-fix

--- src/Enginewerk/FSBundle/Storage/StorageInterface.php
+++ src/Enginewerk/FSBundle/Storage/StorageInterface.php
@@ -6,17 +6,17 @@ namespace Enginewerk\FSBundle\Storage;
  *
  * @author Paweł Czyżewski <pawel.czyzewski@enginewerk.com>
  */
-interface StorageInterface 
+interface StorageInterface
 {
     /**
-     * @param type $key
-     * @param \Symfony\Component\HttpFoundation\File\File $uploadedFile
-     * @return integer File size
+     * @param  type                                        $key
+     * @param  \Symfony\Component\HttpFoundation\File\File $uploadedFile
+     * @return integer                                     File size
      */
     public function put($key, $uploadedFile);
-    
+
     /**
-     * @param string $key
+     * @param  string                                      $key
      * @return \Symfony\Component\HttpFoundation\File\File
      */
     public function get($key);
