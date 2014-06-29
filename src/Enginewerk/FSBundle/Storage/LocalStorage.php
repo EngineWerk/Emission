3842f49d423b23de4cc0b26ea70587580b1251f3
pawski <pawel.czyzewski@enginewerk.com>
2014-06-29 16:54:05 +0200

    Fixed pathname in delete function

--- src/Enginewerk/FSBundle/Storage/LocalStorage.php
+++ src/Enginewerk/FSBundle/Storage/LocalStorage.php
@@ -17,6 +17,7 @@ class LocalStorage implements StorageInterface
     }
 
     /**
+     * Stores File under given key
      *
      * @param  type                                        $key
      * @param  \Symfony\Component\HttpFoundation\File\File $uploadedFile
@@ -32,6 +33,7 @@ class LocalStorage implements StorageInterface
     }
 
     /**
+     * Returns File object for given key
      *
      * @param string $key
      *
@@ -39,37 +41,66 @@ class LocalStorage implements StorageInterface
      */
     public function get($key)
     {
-        $pathname = implode(DIRECTORY_SEPARATOR, array(
-            $this->getStorageRootDirectory(),
-            $this->getDeepDirFromFileName($key),
-            $key)
-                );
+        $pathname = $this->pathname($key);
 
         $file = new File($pathname);
 
         return $file;
     }
 
+    /**
+     * Deletes file with given key
+     *
+     * @param  string           $key
+     * @throws RuntimeException
+     */
     public function delete($key)
     {
-        if (file_exists($key) === false) {
+        $pathname = $this->pathname($key);
+
+        if (file_exists($pathname) === false) {
             throw new RuntimeException(sprintf('File with key "%s" not found.', $key));
         }
-        
-        if (unlink($key) === false) {
+
+        if (unlink($pathname) === false) {
             throw new RuntimeException(sprintf('Can`t delete file with key "%s"', $key));
         }
     }
 
+    /**
+     * Return storage directory root path
+     * @return string
+     */
     private function getStorageRootDirectory()
     {
         return $this->storageRootDirectory;
     }
 
+    /**
+     * Returns nested path based on given name
+     *
+     * @param  string $name
+     * @return string
+     */
     private function getDeepDirFromFileName($name)
     {
         return $name[0] . $name[1] . DIRECTORY_SEPARATOR .
                 $name[2] . $name[3] . DIRECTORY_SEPARATOR .
                 $name[4] . $name[5] . DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * Returns pathname for given key
+     *
+     * @param  string $key
+     * @return string
+     */
+    private function pathname($key)
+    {
+        return implode(DIRECTORY_SEPARATOR, array(
+            $this->getStorageRootDirectory(),
+            $this->getDeepDirFromFileName($key),
+            $key)
+                );
     }
 }
