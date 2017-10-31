23f2d5ca637cc0fa0c003364b31fed5082a808f1
Pawel Czyzewski <pawel.czyzewski@enginewerk.com>
2017-10-31 21:19:52 +0100

    Data migration fix

--- emission/src/Enginewerk/MigrationBundle/Migrations/Version20170112171458_002.php
+++ emission/src/Enginewerk/MigrationBundle/Migrations/Version20170112171458_002.php
@@ -15,11 +15,11 @@ class Version20170112171458_002 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE file DROP FOREIGN KEY FK_8C9F3610A76ED395');
-        $this->addSql('ALTER TABLE file ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, DROP createdAt, DROP updatedAt, CHANGE complete complete TINYINT(1) NOT NULL, CHANGE fileid file_id VARCHAR(16) NOT NULL, CHANGE filehash file_hash VARCHAR(41) NOT NULL, CHANGE expirationdate expiration_date DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE file CHANGE createdAt created_at DATETIME NOT NULL, CHANGE updatedAt updated_at DATETIME NOT NULL, CHANGE complete complete TINYINT(1) NOT NULL, CHANGE fileid file_id VARCHAR(16) NOT NULL, CHANGE filehash file_hash VARCHAR(41) NOT NULL, CHANGE expirationdate expiration_date DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)');
         $this->addSql('ALTER TABLE file_block DROP FOREIGN KEY FK_29982715AFC548A5');
         $this->addSql('DROP INDEX IDX_29982715AFC548A5 ON file_block');
-        $this->addSql('ALTER TABLE file_block ADD file_id INT UNSIGNED DEFAULT NULL, ADD range_start BIGINT UNSIGNED NOT NULL, ADD range_end BIGINT UNSIGNED NOT NULL, DROP rangeStart, DROP rangeEnd, DROP fileId, CHANGE filehash file_hash VARCHAR(41) NOT NULL');
+        $this->addSql('ALTER TABLE file_block CHANGE fileId file_id INT UNSIGNED DEFAULT NULL, CHANGE rangeStart range_start BIGINT UNSIGNED NOT NULL, CHANGE rangeEnd range_end BIGINT UNSIGNED NOT NULL, CHANGE filehash file_hash VARCHAR(41) NOT NULL');
         $this->addSql('ALTER TABLE file_block ADD CONSTRAINT FK_2998271593CB796C FOREIGN KEY (file_id) REFERENCES file (id)');
         $this->addSql('CREATE INDEX IDX_2998271593CB796C ON file_block (file_id)');
     }
@@ -33,11 +33,11 @@ class Version20170112171458_002 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE file DROP FOREIGN KEY FK_8C9F3610A76ED395');
-        $this->addSql('ALTER TABLE file ADD createdAt DATETIME NOT NULL, ADD updatedAt DATETIME NOT NULL, DROP created_at, DROP updated_at, CHANGE complete complete TINYINT(1) DEFAULT \'0\' NOT NULL, CHANGE file_id fileId VARCHAR(16) NOT NULL COLLATE utf8_unicode_ci, CHANGE file_hash fileHash VARCHAR(41) NOT NULL COLLATE utf8_unicode_ci, CHANGE expiration_date expirationDate DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE file CHANGE created_at createdAt DATETIME NOT NULL, CHANGE updated_at updatedAt DATETIME NOT NULL, CHANGE complete complete TINYINT(1) DEFAULT \'0\' NOT NULL, CHANGE file_id fileId VARCHAR(16) NOT NULL COLLATE utf8_unicode_ci, CHANGE file_hash fileHash VARCHAR(41) NOT NULL COLLATE utf8_unicode_ci, CHANGE expiration_date expirationDate DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE file_block DROP FOREIGN KEY FK_2998271593CB796C');
         $this->addSql('DROP INDEX IDX_2998271593CB796C ON file_block');
-        $this->addSql('ALTER TABLE file_block ADD rangeStart BIGINT UNSIGNED NOT NULL, ADD rangeEnd BIGINT UNSIGNED NOT NULL, ADD fileId INT UNSIGNED NOT NULL, DROP file_id, DROP range_start, DROP range_end, CHANGE file_hash fileHash VARCHAR(41) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE file_block CHANGE range_start rangeStart BIGINT UNSIGNED NOT NULL, CHANGE range_end rangeEnd BIGINT UNSIGNED NOT NULL, CHANGE file_id fileId INT UNSIGNED NOT NULL, CHANGE file_hash fileHash VARCHAR(41) NOT NULL COLLATE utf8_unicode_ci');
         $this->addSql('ALTER TABLE file_block ADD CONSTRAINT FK_29982715AFC548A5 FOREIGN KEY (fileId) REFERENCES file (id) ON DELETE CASCADE');
         $this->addSql('CREATE INDEX IDX_29982715AFC548A5 ON file_block (fileId)');
     }
